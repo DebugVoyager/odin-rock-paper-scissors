3c25c6141adc3635b3578ad185e5530020d8f995
debugVoyager <bintangwibawaput@gmail.com>
2024-12-17 16:25:21 +0800

    Implement getHumanChoice function
    Will return either 'rock,' 'paper,' or 'scissors'
    based on the user's input via the window prompt.

--- script.js
+++ script.js
@@ -1,52 +1,23 @@
 function getComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3) + 1; // Added semicolon
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
 
     switch(randomNumber) {
         case 1:
-            return "rock";
+            return "rock"
         case 2:
-            return "paper";
+            return "paper"
         case 3:
-            return "scissors";
+            return "scissors"
     }
 }
 
 function getHumanChoice() {
     let humanChoice = prompt(`Enter "rock", "paper", or "scissors":`);
-    let computerChoice = getComputerChoice()
 
     if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
-        switch(humanChoice) {
-            case "rock":
-                if (computerChoice === "rock") {
-                    alert("Draw!");
-                } else if (computerChoice === "paper") {
-                    alert("You lose!");
-                } else {
-                    alert("You win!");
-                }
-                break
-            case "paper":
-                if (computerChoice === "rock") {
-                    alert("You win!");
-                } else if (computerChoice === "paper") {
-                    alert("Draw!");
-                } else {
-                    alert("You lose!");
-                }
-                break;
-            case "scissors":
-                if (computerChoice === "rock") {
-                    alert("You lose!");
-                } else if (computerChoice === "paper") {
-                    alert("You win!");
-                } else {
-                    alert("Draw!");
-                }
-                break;
-        }
+        return humanChoice;
     }
     else {
-        getHumanChoice()
+        return getHumanChoice();
     }
 }
