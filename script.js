c0d9171567f98e48aa4f5e92044891b14b94abae
DebugVoyager <bintangwibawaput@gmail.com>
2024-12-17 20:53:22 +0800

    Implement the full game
    The `playGame` function will loop through the `playRound` function
    5 times, displaying the winner of each round and the overall winner
    of the game in the console.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0
-let computerScore = 0
-
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3) + 1;
 
@@ -25,31 +22,60 @@ function getHumanChoice() {
     }
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === "rock" && computerChoice === "rock") {
-        console.log("It's a draw!");
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-        console.log("You win! Paper beats Rock.");
-    } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        console.log("You lose! Rock beats Scissors.");
-    } else if (humanChoice === "rock" && computerChoice === "paper") {
-        console.log("You lose! Paper beats Rock.");
-    } else if (humanChoice === "paper" && computerChoice === "paper") {
-        console.log("It's a draw!");
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        console.log("You win! Scissors beats Paper.");
-    } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        console.log("You win! Rock beats Scissors.");
-    } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        console.log("You lose! Scissors beats Paper.");
-    } else if (humanChoice === "scissors" && computerChoice === "scissors") {
-        console.log("It's a draw!");
-    } else {
-        console.log("Invalid choice.");
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0
+    let round = 1
+
+    function displayScore() {
+        console.log(`Human's score: ${humanScore}\nComputer's score: ${computerScore}`);
+    }
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === "rock" && computerChoice === "rock") {
+            console.log(`Round ${round}: It's a draw!`);
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            console.log(`Round ${round}: You win! Paper beats Rock.`);
+            humanScore++;
+        } else if (humanChoice === "scissors" && computerChoice === "rock") {
+            console.log(`Round ${round}: You lose! Rock beats Scissors.`);
+            computerScore++;
+        } else if (humanChoice === "rock" && computerChoice === "paper") {
+            console.log(`Round ${round}: You lose! Paper beats Rock.`);
+            computerScore++;
+        } else if (humanChoice === "paper" && computerChoice === "paper") {
+            console.log(`Round ${round}: It's a draw!`);
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
+            console.log(`Round ${round}: You win! Scissors beats Paper.`);
+            humanScore++;
+        } else if (humanChoice === "rock" && computerChoice === "scissors") {
+            console.log(`Round ${round}: You win! Rock beats Scissors.`);
+            humanScore++;
+        } else if (humanChoice === "paper" && computerChoice === "scissors") {
+            console.log(`Round ${round}: You lose! Scissors beats Paper.`);
+            computerScore++;
+        } else if (humanChoice === "scissors" && computerChoice === "scissors") {
+            console.log(`Round ${round}: It's a draw!`);
+        }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+        round++;
+    }
+
+    if (humanScore > computerScore) {
+        console.log("You win the game!");
+        displayScore();
+    }
+    else if (humanScore < computerScore) {
+        console.log("You lose the game!");
+        displayScore();
+    }
+    else {
+        console.log("it's a draw!");
+        displayScore();
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection)
+playGame();
